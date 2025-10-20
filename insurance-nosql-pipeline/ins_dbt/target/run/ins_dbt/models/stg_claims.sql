5b619ff5d9f1c44922e86eba3db2a03ac6a023e7
Rashmika Fernando <lahfdo20@gmail.com>
2025-10-20 10:06:16 +0530

    Add files via upload

---
+++ insurance-nosql-pipeline/ins_dbt/target/run/ins_dbt/models/stg_claims.sql
@@ -0,0 +1,21 @@
+
+  create or replace   view INSURANCE.RAW.stg_claims
+  
+  
+  
+  
+  as (
+    with raw as(
+    select
+        claim_id,
+        customer_id,
+        to_date(date) as claim_date,
+        amount::float as amount,
+        claim_type,
+        is_fraud:: boolean as is_fraud
+    from INSURANCE.RAW.CLAIMS
+        
+)
+select * from raw
+  );
+
